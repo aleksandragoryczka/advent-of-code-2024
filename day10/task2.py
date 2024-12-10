6935ad781823238543453f78b32c94955f6a4680
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2024-12-10 18:27:49 +0100

    Update task2.py

--- day10/task2.py
+++ day10/task2.py
@@ -11,18 +11,18 @@ for line in lines:
 
 
 visited_dict = {}
-def find_recursively_next_element(el: int, i: int, j: int, init_i: int, init_j: int) -> int:
+def find_recursively_next_element(el: int, i: int, j: int) -> int:
     if el == 9:
         return 1
     search = 0
     if i+1 <= len(board) - 1 and board[i+1][j] == el + 1:
-        search += find_recursively_next_element(el+1, i+1, j, init_i, init_j)
+        search += find_recursively_next_element(el+1, i+1, j)
     if j+1 <= len(board[0]) - 1 and board[i][j+1] == el + 1:
-        search += find_recursively_next_element(el+1, i, j+1, init_i, init_j)
+        search += find_recursively_next_element(el+1, i, j+1)
     if i-1 >= 0 and board[i-1][j] == el + 1:
-        search += find_recursively_next_element(el+1, i-1, j, init_i, init_j)
+        search += find_recursively_next_element(el+1, i-1, j)
     if j-1 >= 0 and board[i][j-1] == el + 1:
-        search += find_recursively_next_element(el+1, i, j-1, init_i, init_j)
+        search += find_recursively_next_element(el+1, i, j-1)
     return search
 
 sum = 0
@@ -30,8 +30,8 @@ counter = 1
 for i, row in enumerate(board):
     for j, el in enumerate(row):
         if el == 0:
-            sum += find_recursively_next_element(0, i, j, i, j)
-            
+            sum += find_recursively_next_element(0, i, j)
+
 print(sum)
 
 
